07a8b6e3a19824488f1ccd0800a9780904021862
Zelenskiy Gena <gena.admin@gmail.com>
2012-11-10 23:15:55 +0200

    +doctrine entity +extended twig

---
+++ src/My/GuestBookBundle/Entity/Post.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace My\GuestBookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * My\GuestBookBundle\Entity\Post
+ *
+ * @ORM\Table(name="guestbook")
+ * @ORM\Entity(repositoryClass="My\GuestBookBundle\Entity\PostRepository")
+ */
+class Post
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $email
+     *
+     * @ORM\Column(name="email", type="string", length=50)
+     */
+    private $email;
+
+    /**
+     * @var string $body
+     *
+     * @ORM\Column(name="body", type="string", length=255)
+     */
+    private $body;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Post
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Post
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string 
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+}
