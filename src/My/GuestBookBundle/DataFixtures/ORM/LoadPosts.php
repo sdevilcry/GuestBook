1e1e567be8bca52002f795280aff1511cba614e1
Zelenskiy Gena <gena.admin@gmail.com>
2012-11-11 01:04:55 +0200

    + added fixtures-bundle + added data fixtures

---
+++ src/My/GuestBookBundle/DataFixtures/ORM/LoadPosts.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace My\GuestBookBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use My\GuestBookBundle\Entity\Post;
+
+class LoadUserData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        for ($i=0; $i<10; $i++)
+        {
+
+        $post1 = new Post();
+        $post1->setEmail('gena@rada.ck.ua');
+        $post1->setBody('Україно, з Днем Рідної мови! Бережімо цей скарб! Шануймося!');
+        $manager->persist($post1);
+        }
+
+
+
+
+        $manager->flush();
+    }
+}
